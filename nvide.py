96d09972b43733397211d864e4680748f6f05036
UnitedCatdom <78739707+CuteBladeYT@users.noreply.github.com>
2022-07-24 23:11:40 +0200

    Fixed Neovide launch options

--- nvide.py
+++ nvide.py
@@ -3,7 +3,7 @@ import sys, subprocess, threading, json
 
 global server, port
 global neovide_path, neovim_path, launcher_path
-global launch_options
+global nvim_launch_opts, neovide_launch_opts
 
 _cfg = open("/usr/bin/nvidecfg/config.json")
 cfg = json.loads(_cfg.read())
@@ -15,23 +15,29 @@ neovide_path = cfg.get("path_to").get("neovide")
 neovim_path = cfg.get("path_to").get("neovim")
 launcher_path = cfg.get("path_to").get("launcher_dir")
 
-launch_options = cfg.get("launch").get("options")
+nvim_launch_opts = cfg.get("launch").get("options").get("nvim")
+neovide_launch_opts = cfg.get("launch").get("options").get("neovide")
 
 
 def listen_server(args = []):
     runcmd = ["/usr/bin/nvidecfg/server.sh", neovim_path, "--headless", "--listen", f"localhost:{str(port)}"]
-    for arg in launch_options:
+    for arg in nvim_launch_opts:
         runcmd.append(arg)
     subprocess.run(runcmd)
 
 def run_nvide(args = []):
     for arg in args:
-        launch_options.append(arg)
+        neovim_launch_options.append(arg)
 
     server = threading.Thread(None, listen_server)
     server.start()
 
-    subprocess.run([neovide_path, f"--remote-tcp=localhost:{str(port)}"])
+    nvd_runcmd = [neovide_path, f"--remote-tcp=localhost:{str(port)}"]
+
+    for arg in neovide_launch_opts:
+        nvd_runcmd.append(arg)
+
+    subprocess.run(nvd_runcmd)
 
 sys.argv.pop(0)
 
